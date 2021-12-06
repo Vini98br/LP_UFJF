b83aa1a39e84221d2d6f9554154264f47ce5c386
Vini98br <viniciuss10@hotmail.com>
2021-12-06 00:30:18 -0300

    .

--- database.pl
+++ database.pl
@@ -1,6 +1,13 @@
-:- dynamic(disciplinaCurso/4).
+% Autores:
+% % Vinicius Soranço 201735003
+% % Arthur Dornelas 201735004
+
+:- dynamic(disciplinaCurso/3).
 :- dynamic(matriculado/3).
 :- dynamic(cursou/3).
+:- dynamic(aluno/2).
+:- dynamic(disciplina/1).
+:- dynamic(curso/1).
 
 disciplina(calculo_1).
 disciplina(calculo_2).
@@ -20,45 +27,45 @@ aluno(joao, cc).
 aluno(camila, cc).
 aluno(danielG, cc).
 aluno(danielM, cc).
+aluno(bruno, cc).
+aluno(henrique, cc).
+aluno(leornardo, cc).
+aluno(rafael, cc).
 aluno(caio, si).
 aluno(pedro, si).
 aluno(victor, si).
 aluno(igor, si).
+aluno(maria, si).
+aluno(gabriel, si).
+aluno(marcelo, si).
+aluno(bruna, si).
+aluno(yasmim, si).
+aluno(isabela, si).
 
+disciplinaCurso(cc, calculo_1, 1).
+disciplinaCurso(cc, algoritmos, 1).
+disciplinaCurso(cc, geo_analitica, 1).
+disciplinaCurso(cc, algebra_linear, 1).
+disciplinaCurso(cc, calculo_2, 2).
+disciplinaCurso(cc, estrutura_de_dados, 2).
+disciplinaCurso(cc, grafos, 3).
+disciplinaCurso(cc, calculo_3, 3).
+disciplinaCurso(si, calculo_1, 1).
+disciplinaCurso(si, algoritmos, 1).
+disciplinaCurso(si, geo_analitica, 1).
+disciplinaCurso(si, algebra_linear, 1).
+disciplinaCurso(si, calculo_2, 2).
+disciplinaCurso(si, estrutura_de_dados, 2).
+disciplinaCurso(si, grafos, 3).
+disciplinaCurso(si, calculo_3, 3).
 
-% disciplinaCurso(curso, disciplina, periodo) - Requisito: matriz curricular de um curso; (disciplinaCurso(cc, X, Y))
-disciplinaCurso(cc, calculo_1, 1, "Obrigatória").
-disciplinaCurso(cc, algoritmos, 1, "Obrigatória").
-disciplinaCurso(cc, geo_analitica, 1, "Obrigatória").
-disciplinaCurso(cc, algebra_linear, 1, "Obrigatória").
-disciplinaCurso(cc, calculo_2, 2, "Obrigatória").
-disciplinaCurso(cc, estrutura_de_dados, 2, "Obrigatória").
-disciplinaCurso(cc, grafos, 3, "Obrigatória").
-disciplinaCurso(cc, calculo_3, 3, "Obrigatória").
-disciplinaCurso(si, calculo_1, 1, "Obrigatória").
-disciplinaCurso(si, algoritmos, 1, "Obrigatória").
-disciplinaCurso(si, geo_analitica, 1, "Obrigatória").
-disciplinaCurso(si, algebra_linear, 1, "Opcional").
-disciplinaCurso(si, calculo_2, 2, "Obrigatória").
-disciplinaCurso(si, estrutura_de_dados, 2, "Obrigatória").
-disciplinaCurso(si, grafos, 3, "Obrigatória").
-disciplinaCurso(si, calculo_3, 3, "Opcional").
-
-% matriculado(nomeEstudante, disciplina, nota) - Requisito: histórico escolar de um estudante; (matriculado(vinicius, X, Y))
 cursou(vinicius, calculo_1, 70).
 cursou(vinicius, algoritmos, 70).
 cursou(vinicius, geo_analitica, 70).
 cursou(vinicius, algebra_linear, 70).
 cursou(vinicius, estrutura_de_dados, 59).
-cursou(arthur, calculo_1, 70).
+cursou(arthur, calculo_1, 61).
 cursou(arthur, algoritmos, 70).
 cursou(arthur, geo_analitica, 70).
 cursou(arthur, algebra_linear, 70).
 cursou(arthur, estrutura_de_dados, 59).
-
-matriculado(vinicius, calculo_2, "-").
-matriculado(vinicius, estrutura_de_dados, "-").
-matriculado(arthur, calculo_2, "-").
-matriculado(arthur, estrutura_de_dados, "-").
-
-
--- main.pl
+++ main.pl
@@ -1,70 +1,323 @@
-
-% matrizCurso(X) :- 
-%   disciplinaCurso(X, _, _).
-
-% Escrever 0 para com nota e 1 para sem nota
-% N == 0 ->  
-% aprovado(X, disciplina, Y) ;
-% aprovado(X, disciplina)
+% Autores:
+% % Vinicius Soranço 201735003
+% % Arthur Dornelas 201735004
 
 :- consult('database.pl').
 
 menu :- repeat,
   write('Escolha uma opção:'),nl,
+  write('0. Gerenciar dados'),nl,
   write('1. Historico escolar de um estudante'),nl,
   write('2. Matriz curricular de um curso'),nl,
   write('3. Relação de estudantes que já cursaram uma dada disciplina'),nl,
   write('4. Relação de disciplinas que faltam ser cursadas para um dado estudante'),nl,
+  write('5. Relacao de estudantes de um dado curso'), nl,
+  write('6. Relacao de cursos que cont�m uma dada disciplina'),nl,
+  write('7. Fechar programa'),nl,
   read(Choice),
   run_opts(Choice),
   break.
 
 
 run_opts(N) :-
+  N == 0 -> menu_data();
+
   N == 1 -> write('Digite o nome do estudante:'), read(X), option_one(X) ;
 
-  N == 2 -> write('Digite o nome do curso (cc ou si):'), read(Curso), option_two(Curso) ; 
+  N == 2 -> write('Digite o nome do curso (cc ou si):'), read(Curso), option_two(Curso) ;
 
-  N == 3 -> write('Digite o nome da disciplina:'), read(Disciplina), option_three(Disciplina) ; 
+  N == 3 -> write('Digite o nome da disciplina:'), read(Disciplina), option_three(Disciplina) ;
 
-  N == 4 -> write('Digite o nome do estudante:'), read(Estudante), option_four(Estudante) ; 
+  N == 4 -> write('Digite o nome do estudante:'), read(Estudante), option_four(Estudante) ;
 
-  N == 5 -> write('').
+  N == 5 -> write('Digite o nome do curso:'), read(Curso), option_five(Curso);
 
-option_one(Estudante) :- 
-  display_historic_table_header,
-  cursou(Estudante, Disciplina, Nota), aluno(Estudante, Curso), disciplinaCurso(Curso, Disciplina, Periodo, _),
-  (Nota > 60 -> format("|~t~a~t~22||~t~a~t~22+|~t~a~t~22+|~t~a~t~22+|~n", [Disciplina, Nota, Periodo, "APROVADO"]) ; 
+  N == 6 -> write('Digite o nome da disciplina:'), read(Disciplina), option_six(Disciplina); 
+
+  N == 7 -> break.
+
+menu_data() :-
+  write('Escolha uma opção:'),nl,
+  write('0. Cadastrar Estudante'),nl,
+  write('1. Remover Estudante'),nl,
+  write('2. Editar Estudante'),nl,
+  write('3. Cadastrar Curso'),nl,
+  write('4. Remover Curso'),nl,
+  write('5. Editar Curso'), nl,
+  write('6. Cadastrar Disciplina'),nl,
+  write('7. Remover Disciplina'),nl,
+  write('8. Editar Disciplina'),nl,
+  write('9. Cadastrar Disciplina em Curso'),nl,
+  write('10. Remover Disciplina em Curso'),nl,
+  write('11. Editar Disciplina em Curso'),nl,
+  write('12. Cadastrar Estudante em Disciplina'),nl,
+  write('13. Remover Estudante em Disciplina'),nl,
+  write('14. Editar Estudante em Disciplina'),nl,
+  read(Choice),
+  run_opts_data(Choice),
+  break.
+
+run_opts_data(N) :-
+  N == 0 -> add_estudante ;
+  N == 1 -> delete_estudante ;
+  N == 2 -> edit_estudante ;
+  N == 3 -> add_curso;
+  N == 4 -> delete_curso;
+  N == 5 -> edit_curso;
+  N == 6 -> add_disciplina;
+  N == 7 -> delete_disciplina;
+  N == 8 -> edit_disciplina;
+  N == 9 -> add_disciplina_curso;
+  N == 10 -> delete_disciplina_curso;
+  N == 11 -> edit_disciplina_curso;
+  N == 12 -> add_estudante_disciplina;
+  N == 13 -> delete_estudante_disciplina;
+  N == 14 -> edit_estudante_disciplina.
+
+add_estudante :-
+  write('Nome do estudante:'), nl,
+  read(X),
+  write('Nome do curso:'), nl,
+  read(Y),
+  write('Aluno Cadastrado'), nl,
+  assert(aluno(X, Y)),
+  menu,
+  fail.
+
+edit_estudante :-
+  write('Nome do estudante:'), nl,
+  read(X),
+  write('Nome do curso:'), nl,
+  read(Y),
+  retract(aluno(X,Y)),
+  write('Novo nome do estudante:'), nl,
+  read(W),
+  write('Novo curso do estudante:'), nl,
+  read(Z),
+  write('Aluno editado'), nl,
+  write(W), nl,
+  assert(aluno(W,Z)),
+  menu,
+  fail.
+
+delete_estudante :-
+  write('Nome do estudante:'), nl,
+  read(X),
+  write('Nome do curso:'), nl,
+  read(Y),
+  retract(aluno(X,Y)),
+  write('Aluno excluido'),
+  menu,
+  fail.
+
+add_curso :-
+  write('Nome do curso:'), nl,
+  read(X),
+  assert(curso(X)),
+  write('Curso Cadastrado'), nl,
+  write(X), nl,
+  menu,
+  fail.
+
+edit_curso :-
+  write('Nome do curso:'), nl,
+  read(X),
+  retract(curso(X)),
+  write('Novo nome do curso:'), nl,
+  read(X),
+  assert(curso(X)),
+  write('Curso editado'), nl,
+  write(X), nl,
+  menu,
+  fail.
+
+delete_curso :-
+  write('Nome do curso:'), nl,
+  read(X),
+  retract(curso(X)),
+  retractall(aluno(_, X)),
+  write('Curso excluido'),
+  menu,
+  fail.
+
+add_disciplina :-
+  write('Nome da disciplina:'), nl,
+  read(X),
+  write('Disciplina Cadastrada'), nl,
+  write(X), nl,
+  assert(disciplina(X)),
+  menu,
+  fail.
+
+edit_disciplina :-
+  write('Nome da disciplina:'), nl,
+  read(X),
+  retract(disciplina(X)),
+  write('Novo nome da disciplina:'), nl,
+  read(X),
+  write('Disciplina editada'), nl,
+  write(X), nl,
+  assert(disciplina(X)),
+  menu,
+  fail.
+
+delete_disciplina :-
+  write('Nome da disciplina:'), nl,
+  read(X),
+  retract(disciplina(X)),
+  retractall(cursou(_, X, _)),
+  write('Disciplina excluida'),
+  menu,
+  fail.
+
+add_disciplina_curso :-
+  write('Nome da disciplina:'), nl,
+  read(X),
+  write('Nome do curso:'), nl,
+  read(Y),
+  write('Periodo:'), nl,
+  read(Z),
+  assert(disciplinaCurso(Y, X, Z)),
+  write('Adicionada'),
+  menu,
+  fail.
+
+edit_disciplina_curso :-
+  write('Nome da disciplina:'), nl,
+  read(X),
+  write('Nome do curso:'), nl,
+  read(Y),
+  write('Periodo:'), nl,
+  read(Z),
+  retract(disciplinaCurso(Y, X, Z)),
+  write('Novo nome da disciplina:'), nl,
+  read(X),
+  write('Novo nome do curso:'), nl,
+  read(Y),
+  write('Novo periodo:'), nl,
+  read(Z),
+  assert(disciplinaCurso(Y, X, Z)),
+  write('Editada'),
+  menu,
+  fail.
+
+delete_disciplina_curso :-
+  write('Nome da disciplina:'), nl,
+  read(X),
+  write('Nome do curso:'), nl,
+  read(Y),
+  write('Periodo:'), nl,
+  read(Z),
+  retract(disciplinaCurso(Y, X, Z)),
+  write('Deletada'),
+  menu,
+  fail.
+
+add_estudante_disciplina :-
+  write('Nome do estudante:'), nl,
+  read(X),
+  write('Nome da disciplina:'), nl,
+  read(Y),
+  write('Nota:'), nl,
+  read(Z),
+  assert(cursou(X, Y, Z)),
+  write('Adicionado'),
+  menu,
+  fail.
+
+edit_estudante_disciplina :-
+  write('Nome do estudante:'), nl,
+  read(X),
+  write('Nome da disciplina:'), nl,
+  read(Y),
+  write('Nota:'), nl,
+  read(Z),
+  retract(cursou(X, Y, Z)),
+  write('Novo nome do estudante:'), nl,
+  read(X),
+  write('Novo nome da disciplina:'), nl,
+  read(Y),
+  write('Nova nota:'), nl,
+  read(Z),
+  assert(cursou(X, Y, Z)),
+  write('Editado'),
+  menu,
+  fail.
+
+delete_estudante_disciplina :-
+  write('Nome do estudante:'), nl,
+  read(X),
+  write('Nome da disciplina:'), nl,
+  read(Y),
+  write('Nota:'), nl,
+  read(Z),
+  retract(cursou(X, Y, Z)),
+  write('Deletado'),
+  menu,
+  fail.
+
+option_one(Estudante) :-
+  format("|~t~a~t~34||~t~a~t~22+|~t~a~t~22+|~t~a~t~22+|", ["Disciplina", "Nota", "Período", "Status"]), nl,
+  format("|~`-t~88||~n"),
+  cursou(Estudante, Disciplina, Nota), aluno(Estudante, Curso), disciplinaCurso(Curso, Disciplina, Periodo),
+  (Nota > 60 -> format("|~t~a~t~22||~t~a~t~22+|~t~a~t~22+|~t~a~t~22+|~n", [Disciplina, Nota, Periodo, "APROVADO"]) ;
   format("|~t~a~t~22||~t~a~t~22+|~t~a~t~22+|~t~a~t~22+|~n", [Disciplina, Nota, Periodo, "REPROVADO"])),
-  % format("|~t~a~t~22||~t~a~t~22+|~t~a~t~22+|~t~a~t~22+|~n", [Disciplina, Nota, Periodo, "MATRICULADO"])),
   fail.
 
-option_two(Curso) :- 
-  format("|~t~a~t~28||~t~a~t~22+|~t~a~t~22+|", ["Disciplinas", "Período", "Obrigatoriedade"]), nl,
+option_two(Curso) :-
+  format("|~t~a~t~28||~t~a~t~22+|", ["Disciplinas", "Período"]), nl,
+  format("|~`-t~44||~n"),
+  disciplinaCurso(Curso, Disciplina, Periodo),
+  format("|~t~a~t~22||~t~a~t~22+|~n", [Disciplina, Periodo]),
+  fail.
+
+option_three(Disciplina) :-
+  write("Deseja incluir um critério de seleção por nota s/n?"), read(R),
+  R == 's' -> write("Digite a nota: "), read(Grade),
+  format("|~t~a~t~28||~t~a~t~22+|~t~a~t~22+|", ["Disciplina", "Estudante", "Nota"]), nl,
   format("|~`-t~66||~n"),
-  disciplinaCurso(Curso, Disciplina, Periodo, Obrigatoriedade),
-  format("|~t~a~t~22||~t~a~t~22+|~t~a~t~22+|~n", [Disciplina, Periodo, Obrigatoriedade]),
-  fail.
-
-option_three(Disciplina) :- 
-  % write("Deseja incluir um critério de seleção por nota s/n?"), read(R),
-  % R == "s" -> write("Digite a nota: "), read(Grade), write(Grade) ; write('').
+  cursou(Estudante, Disciplina, Grade),
+  format("|~t~a~t~22||~t~a~t~22+|~t~a~t~22+|~n", [Disciplina, Estudante, Grade]),
+  fail ;
   format("|~t~a~t~28||~t~a~t~22+|~t~a~t~22+|", ["Disciplina", "Estudante", "Nota"]), nl,
   format("|~`-t~66||~n"),
   cursou(Estudante, Disciplina, Nota),
   format("|~t~a~t~22||~t~a~t~22+|~t~a~t~22+|~n", [Disciplina, Estudante, Nota]),
   fail.
 
+disciplinaEstudante(Estudante, Disciplina) :-
+  aluno(Estudante, C), disciplinaCurso(C, Disciplina, _).
 
-option_four(Estudante) :- 
-  findall(Disciplina, aluno(Estudante, disciplinaCurso(Curso, Disciplina)), Disciplinas),
-  write(Disciplinas).
+option_four(Estudante) :-
+  findall(DisciplinaNaoCursada, disciplinaEstudante(Estudante, DisciplinaNaoCursada), DisciplinasCurso),
+  findall(DisciplinaCursada, cursou(Estudante, DisciplinaCursada, _), DisciplinasCursadas),
+  subtract(DisciplinasCurso, DisciplinasCursadas, R),
+  write("Disciplinas que faltam ser cursadas: "),
+  writeln(R),
+  menu.
 
+func(Curso, Estudante) :- 
+  aluno(Estudante, Curso), cursou(Estudante, _,  _).
 
-display_historic_table_header :- 
-  format("|~t~a~t~34||~t~a~t~22+|~t~a~t~22+|~t~a~t~22+|", ["Disciplina", "Nota", "Período", "Status"]), nl,
-  format("|~`-t~88||~n").
+option_five(Curso) :-
+  write("Se deseja incluir um critério de seleção por nota digite 'n' ou se desajar incluir critério de seleção por IRA, digite 'i', senão aperte 'c' para buscar sem criterio"), read(R),
+  R == 'n' -> write("Digite a nota: "), read(Nota), nl, 
+  findall(Estudante, func(Curso, Estudante), Notas),
+  write('Alunos: '),
+  sort(Notas, X),
+  writeln(X),
+  fail ; 
+  format("|~t~a~t~27||~t~a~t~22+|", ["Curso", "Estudante"]), nl,
+  format("|~`-t~44||~n"),
+  aluno(Estudante, Curso),
+  format("|~t~a~t~22||~t~a~t~22+|~n", [Curso, Estudante]),
+  fail.
+
+option_six(Disciplina) :-
+  format("|~t~a~t~36||~t~a~t~22+|", ["Disciplina", "Cursos"]), nl,
+  format("|~`-t~44||~n"),
+  disciplinaCurso(Curso, Disciplina, _), curso(Curso),
+  format("|~t~a~t~22||~t~a~t~22+|~n", [Disciplina, Curso]),
+  fail.
 
 ?- menu.
-
-% swipl /home/vinicius/Code/UFJF/LP/test.pl
